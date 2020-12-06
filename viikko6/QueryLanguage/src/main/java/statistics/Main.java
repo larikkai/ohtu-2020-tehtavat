8b2f821f7b04c5bf19afbf19974e8089fb9bf0c1
larikkai <kai.larikka@kolumbus.fi>
2020-12-06 21:39:15 +0200

    Add week6 tehtava2

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,13 +8,25 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
-        Matcher m = new And( new HasAtLeast(5, "goals"),
+        /*Matcher m = new And( new HasAtLeast(5, "goals"),
                              new HasAtLeast(5, "assists"),
                              new PlaysIn("PHI")
+        );*/
+        
+        /*Matcher m = new And( 
+            new Not( new HasAtLeast(1, "goals") ), 
+            new PlaysIn("NYR")
+        );*/
+        
+        /*Matcher m = new And( 
+            new HasFewerThan(1, "goals"), 
+            new PlaysIn("NYR")
         );
         
         for (Player player : stats.matches(m)) {
             System.out.println(player);
-        }
+        }*/
+        
+        System.out.println(stats.matches(new All()).size());
     }
 }
